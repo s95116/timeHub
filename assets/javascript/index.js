efdb952516e4ca71bfaa0ad6523f9a8a0cabffa0
Seth Keel <seth.keel@gmail.com>
2016-06-16 19:43:13 -0400

    first commit

---
+++ assets/javascript/index.js
@@ -0,0 +1,18 @@
+$('[data-toggle="popover"]').popover();
+
+
+// $(document).ready(function(){
+	
+
+
+	// $('.toggle').on('click', function() {
+	//   $('.container').stop().addClass('active');
+	// });
+
+	// $('.close').on('click', function() {
+	//   $('.container').stop().removeClass('active');
+	// });
+
+
+
+// });
